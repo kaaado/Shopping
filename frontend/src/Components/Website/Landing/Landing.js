69f8dfc27d4acf1df316bf44a76f6d92ae591aa6
Yacine kermame <86516881+kaaado@users.noreply.github.com>
2025-01-30 11:08:14 +0100

    Update Landing.js

--- frontend/src/Components/Website/Landing/Landing.js
+++ frontend/src/Components/Website/Landing/Landing.js
@@ -10,7 +10,7 @@ export default function Landing() {
 
  const getRandomProductId = () => {
   const storedIds = JSON.parse(localStorage.getItem("ids")) || [];
-  if (storedIds.length === 0) return null;
+  if (storedIds.length === 0) return storedIds.push(1);
   const randomIndex = Math.floor(Math.random() * storedIds.length);
   return storedIds[randomIndex];
 };
